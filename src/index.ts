32f69281362055da100d57571ef1f5797c9e100a
krepyshev <mail@krepyshev.pro>
2024-03-14 14:29:41 +0500

    Add README and types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 import './scss/styles.scss';
+import { API_URL, CDN_URL } from './utils/constants';
 
+import { Api } from './components/base/api';
+
+const api = new Api(API_URL);
+
+api
+	.get('/product/b06cde61-912f-4663-9751-09956c0eed67')
+	.then((result) => console.log(result));
---
+++ src/types/index.ts
@@ -0,0 +1,48 @@
+export interface ICard {
+  id: string;
+  description?: string;
+  image?: string;
+  title: string;
+  category?: string;
+  price: number;
+}
+
+export interface IForm {
+  payMethod?: string;
+  address?: string;
+  email?: string;
+  phone?: string;
+  image?: string;
+  title?: string;
+  description?: string;
+}
+
+export interface IModalData {
+  content: HTMLElement;
+}
+
+export interface IOrderResult {
+  id: string;
+}
+
+export interface IEvents {
+  on(): void;
+  emit(): void;
+  trigger(): () => void;
+}
+
+export interface IShopAPI {
+  getCardList: () => Promise<ICard[]>;
+  getCardItem: () => Promise<ICard>;
+  orderCards: () => Promise<IOrderResult>;
+}
+
+export interface IAppState {
+  catalog: ICard[];
+  basket: string[];
+  order: IForm;
+}
+
+export type Category = {
+  category: string[];
+};
